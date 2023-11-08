c60336c71afe19a327cdbd123aeefc6320444fa5
ignaciocampillosserrano <ignacio.campillos.serrano@cieep.com>
2023-11-08 12:00:04 +0100

    Acabado ejercicio repaso 1

--- app/src/main/java/ignacio/campillos/androidstudio_cigarros_repaso/MainActivity.java
+++ app/src/main/java/ignacio/campillos/androidstudio_cigarros_repaso/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -12,12 +13,14 @@ import android.view.View;
 
 import ignacio.campillos.androidstudio_cigarros_repaso.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private int cont;
+    private int cost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,39 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        cont = 20;
+        cost = 5;
+
+        binding.lbLeftActivityMain.setText(String.valueOf(cont));
+        binding.lbCostActivityMain.setText(String.valueOf(cost) + "€");
+
+        binding.imgPackActivityMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cont--;
+                if (cont <= 0){
+                    cont = 20;
+                    cost += 5;
+                }
+
+                binding.lbLeftActivityMain.setText(String.valueOf(cont));
+                binding.lbCostActivityMain.setText(String.valueOf(cost) + "€");
+            }
+        });
+
+    }
+    //A LA HORA DE INCLINAR EL MOVIL SE GUARDA LA INFORMACION (Lista) EN UN BUNDLE
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+    }
+
+    //AL VOLVER SE CARGA LA INFORMACION DE DICHO BUNDLE EN LA LISTA y SE ACTUALIZA EL ADAPTER CON TODA LA LISTA (Desde index 0 al tamaño de la lista)
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
 
     }
 }
